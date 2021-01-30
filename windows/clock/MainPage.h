d167f33d0bc056cab759453189433e26681dc4e1
gongbaodd@Inspiron_PS <gongbaodd@qq.com>
2021-01-30 18:35:17 +0800

    first commit

---
+++ windows/clock/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::clock::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::clock::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
